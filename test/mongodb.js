b7d39389e6168266db63b431ca036d8f37e1cc3f
Bogdan <bogdankrajnik95@gmail.com>
2024-05-17 19:02:22 +0300

    hello

--- test/mongodb.js
+++ test/mongodb.js
@@ -1,7 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const DB_URI = "mongodb+srv://Bogdan:b77Ix808hCvn9tXm@mycluster.zizq2do.mongodb.net/?retryWrites=true&w=majority&appName=myCluster";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(DB_URI);
 
 async function run() {
